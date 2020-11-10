adf1b438dcdfca3c418d63b7426897ce58aac019
Anjani Sowmya Bollapragada <asbollapragada@gmail.com>
2020-11-10 17:19:16 +0530

    First set of Files

---
+++ app/src/main/java/com/asbollapragada/firebase_store/Upload.java
@@ -0,0 +1,13 @@
+package com.asbollapragada.firebase_store;
+
+public class Upload {
+    String link;
+    public Upload(String link) {
+        this.link = link;
+    }
+    public Upload() {
+    }
+    public String getLink() {
+        return link;
+    }
+}
